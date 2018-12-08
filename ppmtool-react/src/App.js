eb60e52413321a2f6767b75115cd94dad91f9e74
salvescoding <salves2383@gmail.com>
2018-12-08 12:10:11 +0100

    Install and implement react route dom

--- ppmtool-react/src/App.js
+++ ppmtool-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
@@ -6,10 +7,12 @@ import Header from './components/Layout/Header';
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Header />
-				<Dashboard />
-			</div>
+			<Router>
+				<div className="App">
+					<Header />
+					<Dashboard />
+				</div>
+			</Router>
 		);
 	}
 }
