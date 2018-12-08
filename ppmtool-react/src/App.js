70cdb552d11e0ee2a1a71cfbb7f6d946e533b137
salvescoding <salves2383@gmail.com>
2018-12-08 16:20:58 +0100

    Setup redux in react app

--- ppmtool-react/src/App.js
+++ ppmtool-react/src/App.js
@@ -4,17 +4,21 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import AddProject from './components/Project/AddProject';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
 	render() {
 		return (
-			<Router>
-				<div className="App">
-					<Header />
-					<Route exact path="/dashboard" component={Dashboard} />
-					<Route exact path="/addProject" component={AddProject} />
-				</div>
-			</Router>
+			<Provider store={store}>
+				<Router>
+					<div className="App">
+						<Header />
+						<Route exact path="/dashboard" component={Dashboard} />
+						<Route exact path="/addProject" component={AddProject} />
+					</div>
+				</Router>
+			</Provider>
 		);
 	}
 }
