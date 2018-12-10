79eeb92262fc38c07fea66847c4f918836972fda
salvescoding <salves2383@gmail.com>
2018-12-10 15:43:09 +0100

    Add classnames package, create error messages for invalid input in form when add new project

--- ppmtool-react/src/store.js
+++ ppmtool-react/src/store.js
@@ -2,21 +2,22 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const initialState = {};
+const initalState = {};
 const middleware = [thunk];
 
 let store;
+
 if (window.navigator.userAgent.includes('Chrome')) {
 	store = createStore(
 		rootReducer,
-		initialState,
+		initalState,
 		compose(
 			applyMiddleware(...middleware),
 			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 		)
 	);
 } else {
-	store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
+	store = createStore(rootReducer, initalState, compose(applyMiddleware(...middleware)));
 }
 
 export default store;
