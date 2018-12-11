41dc7a68a306916bd3b1a86be3c263cc287f1ee4
salvescoding <salves2383@gmail.com>
2018-12-11 11:23:06 +0100

    Create route to update existing project

--- ppmtool-react/src/App.js
+++ ppmtool-react/src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Layout/Header';
 import AddProject from './components/Project/AddProject';
 import { Provider } from 'react-redux';
 import store from './store';
+import UpdateProject from './components/Project/UpdateProject';
 
 class App extends Component {
 	render() {
@@ -16,6 +17,7 @@ class App extends Component {
 						<Header />
 						<Route exact path="/dashboard" component={Dashboard} />
 						<Route exact path="/addProject" component={AddProject} />
+						<Route exact path="/updateProject/:id" component={UpdateProject} />
 					</div>
 				</Router>
 			</Provider>
---
+++ ppmtool-react/src/components/Project/UpdateProject.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class UpdateProject extends Component {
+	render() {
+		return <div />;
+	}
+}
+export default UpdateProject;
