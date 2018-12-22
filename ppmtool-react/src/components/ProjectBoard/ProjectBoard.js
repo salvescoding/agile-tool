2e7572c6a6ba4623c590a9365bdf3f3d43717777
salvescoding <salves2383@gmail.com>
2018-12-22 12:34:57 +0100

    Add Projectboard link and route for project board dashboard

---
+++ ppmtool-react/src/components/ProjectBoard/ProjectBoard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ProjectBoard extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Welcome to your ProjectBoard</h1>
+			</div>
+		);
+	}
+}
+export default ProjectBoard;
